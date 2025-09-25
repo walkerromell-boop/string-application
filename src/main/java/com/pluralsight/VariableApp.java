049f47ff570936ebc289c8503bb7dce58d021f7b
Romell Walker <walkerromell@gmail.com>
2025-09-25 11:34:39 -0400

    1st Commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String color= "red";
+        System.out.println("The answer is:" +color);
+    }
+}
