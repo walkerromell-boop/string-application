7cc4a885a4a1cf9d9857a9fdaad129f9c27f8192
Romell Walker <walkerromell@gmail.com>
2025-09-25 12:36:43 -0400

    i really finished exercise 2 now

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+        String mac="macbook pro's";
+        int quan= 10;
+        double price= 200;
+        System.out.println("You bought"+" "+quan+" "+mac+" for $"+price+"!");
+    }
+}
