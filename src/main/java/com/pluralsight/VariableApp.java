2019ae41a6e21272204286a2c15db40925bc3d7e
Romell Walker <walkerromell@gmail.com>
2025-09-25 12:20:52 -0400

    finished exercise 2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -4,5 +4,38 @@ public class VariableApp {
     public static void main(String[] args) {
         String color= "red";
         System.out.println("The answer is:" +color);
+        int year= (2025);
+        System.out.println("The answer is:" + year);
+        char intial ='A';
+        System.out.println("The answer is:" + intial);
+        boolean pets = true;
+        System.out.println("The answer is:"+pets);
+        String nicesaying= "You got it";
+        System.out.println("The answer is:"+ nicesaying);
+
+        int days= (7);
+
+        float price = 4.99f;
+
+        char letter= 'R';
+
+        boolean raining = false;
+
+        System.out.println("There are"+days+ " "+"in a week");
+        System.out.println("The coffee cost"+price+".");
+        System.out.println(letter+" is my favorite letter");
+        System.out.println("it's"+" "+raining+" that it's raining");
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
